8adb288323c7fb66218312ec0857e2d45caf5f35
Aluno <pedromury.pm@gmail.com>
2020-02-27 22:41:59 -0300

    Criei um novo projeto

---
+++ Aulas_De_Desenvolvimento_de_Sistema/Contas/JavaApplication1/src/javaapplication1/Soma.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+public class Soma extends JFrame{
+    JLabel rotulo1, rotulo2, exibir;
+    JTextField texto1, texto2;
+    JButton soma, subi, divi, multii;
+public Soma(){
+    super("Exemplo de contas");
+    Container tela = getContentPane();
+    setLayout(null);
+        rotulo1 = new  JLabel("1º Número: ");
+        rotulo2 = new JLabel("2º Número: ");
+        texto1 = new JTextField(5);
+        texto2 = new JTextField(5);
+        ;
+        soma = new JButton("Soma");
+        subi = new JButton("Subtração");
+        divi = new JButton("Divisão");
+        multii = new JButton("Multiplicação");
+        
+        
+   rotulo1.setBounds(50, 20, 100, 20);
+   rotulo2.setBounds(50, 60, 100, 20);
+   texto1.setBounds(120, 20, 200, 20);
+   texto2.setBounds(120, 60, 200, 20);
+   exibir.setBounds(50, 120, 200, 20);
+   soma.setBounds(50, 100, 80, 20);
+   subi.setBounds(70, 100, 80, 20);
+   multii.setBounds(90, 100, 80, 20);
+   divi.setBounds(110, 100, 80, 20);
+   
+   soma.addActionListener(
+           new ActionListener(){
+               public void actionPerformed(ActionEvent e){
+                   int numero1, numero2, soma;
+                   soma = 0;
+                   numero1 = Integer.parseInt(texto1.getText());
+                   numero2 = Integer.parseInt(texto2.getText());
+                   soma = numero1 + numero2;
+                   exibir.setVisible(true);
+                   exibir.setText("A soma é: " +soma);
+               }
+           }
+                   );   
+                   
+    subi.addActionListener(
+           new ActionListener(){
+               public void actionPerformed(ActionEvent e){
+                   int numero1, numero2, sub;
+                   sub = 0;
+                   numero1 = Integer.parseInt(texto1.getText());
+                   numero2 = Integer.parseInt(texto2.getText());
+                   sub = numero1 - numero2;
+                   exibir.setVisible(true);
+                   exibir.setText("A subtração é: " +sub );
+               }
+           }
+                   );   
+                   
+    divi.addActionListener(
+           new ActionListener(){
+               public void actionPerformed(ActionEvent e){
+                   int numero1, numero2, div;
+                   div = 0;
+                   numero1 = Integer.parseInt(texto1.getText());
+                   numero2 = Integer.parseInt(texto2.getText());
+                   div = numero1 / numero2;
+                   exibir.setVisible(true);
+                   exibir.setText("A soma é: " +div);       
+               }
+           }
+);  
+                   
+                   
+    multii.addActionListener(
+           new ActionListener(){
+               public void actionPerformed(ActionEvent e){
+                   int numero1, numero2, multi;
+                   multi = 0;
+                   numero1 = Integer.parseInt(texto1.getText());
+                   numero2 = Integer.parseInt(texto2.getText());
+                   multi = numero1 * numero2;
+                   exibir.setVisible(true);
+                   exibir.setText("A soma é: " +multi);               
+                   
+               }
+           }
+   );
+   exibir.setVisible(false);
+   
+   tela.add(rotulo1);
+   tela.add(rotulo2);
+   tela.add(texto1);
+   tela.add(texto2);
+   tela.add(exibir);
+   tela.add(soma);
+   tela.add(subi);
+   tela.add(divi);
+   tela.add(multii);
+   
+   setSize(450, 300);
+   setVisible(true);
+   
+
+    public static void main(String args[]){
+    Soma app = new Soma();
+    app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+         
+  
+            
